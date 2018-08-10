35622774c13503a232485f4c49a6c06e24b0d12a
Filipe Guarnieri <consultor5a.guarnieri@webmotors.com.br>
2018-08-10 12:27:07 -0300

    NEW Version 2.0.0

--- README.md
+++ README.md
@@ -1,2 +1,47 @@
-# react-maskfy
+# Maskfy (2.0.0) - Input Mask Simple
 A React library without a dependency... Very simple to install and use. With only 1kb (gzip) code, IE Compatibility, it's also well accepted on mobile devices
+## Install / Usage
+Install npm Maskfy
+```node
+npm i react-maskfy
+```
+### Import Component
+```javascript
+import Maskfy from 'react-maskfy';
+```
+### Usage Component
+```javascript
+<Maskfy mask={'(99) 9999-99999'}>
+  <input id="Phone" name="Phone" />
+</Maskfy>
+```
+or
+```javascript
+<Maskfy mask={'(99) 9999-99999'}>
+  <TextField id="Phone" label="Phone" />
+</Maskfy>
+```
+## Component (props)
+```javascript
+<Maskfy mask={'999.999.999,99'} reverse={true} minSize={3} defaultValue={'123'} letters={false} after={handleEvent}>
+  <input id="Money" name="Money" />
+</Maskfy>
+```
+### mask
+{String: undefined} **__(required)__ selector input**
+### reverse
+{Boolean: false} **reverse typing**
+### minSize
+{Number: undefined} **minimum digits**
+### defaultValue
+{String: undefined} **initial value**
+### letters
+{Boolean: false} **allowed letters**
+### after
+{Function: undefined} **after input event**
+
+## Source
+[Github](https://github.com/figuarnieri/maskfy) | [Vanilla npm](https://www.npmjs.com/package/maskfy) | [Example](https://figuarnieri.github.io/maskfy/) | [@figuarnieri](https://twitter.com/figuarnieri)
+## License
+The MIT License
+Copyright 2018 © [Filipe Guarnieri](https://figuarnieri.github.io/)
---
+++ maskfy-vanilla.js
@@ -0,0 +1,146 @@
+
+/**
+ * @author: figuarnieri
+ * @copyright: The MIT License
+ * @name Maskfy
+ * @description: A Javascript library without a dependency of jQuery, Zepto, and etc ... Very simple to install and use. With only 1kb (gzip) code, it's also well accepted on mobile devices
+ * @since: 2018
+ * @version: 2.0.0
+ */
+"use strict";
+
+class Maskfy extends React.Component{
+  /**
+   * @param {String || Object} Selector String
+   * @return {String || Object}
+   */
+  constructor(props){
+    super(props);
+    this.state = {
+      mask: this.props.mask, // {String} Input mask
+      reverse: this.props.reverse, // {Boolean} Content insert invert (like coins)
+      minSize: this.props.minSize, // {Number} Minimum number of characters
+      defaultValue: this.props.defaultValue, // {String} Initial value
+      letters: this.props.letters, // {Boolean} Allowed alpha characters
+      after: this.props.after, // {Function} Function after insert new input value
+    }
+  }
+  componentDidMount(){
+    const _tag = this._reactInternalFiber.child.stateNode;
+    const _input = _tag.tagName==='INPUT' ? _tag : _tag._reactInternalFiber.child.stateNode.querySelector('input');
+    if(!_input){
+      return console.error(new Error('Maskfy: Insert tag selector required. Ex.: Maskfy({tag: "[data-maskfy]"})'))
+    } else {
+      this.tag = _input;
+      this.init();
+    };
+  }
+  
+  /**
+   * function to execute foreach of objectTag
+   * @param {String} input
+   * @return {String}
+   */
+  init(){
+    this.tag.addEventListener('input', e => this.format(e));
+    const _value = this.tag.value.trim();
+    if(this.state.defaultValue || _value!==''){
+      this.tag.value = this.tag.value || this.state.defaultValue || this.tag.dataset.maskfyValue;
+    }
+    this.event(this.tag);
+  }
+  
+  /**
+   * dispatchEvent Input
+   * @param {String} input
+   * @return {String}
+   */
+  event(tag){
+    if(window.navigator.msPointerEnabled){
+      const event = document.createEvent("Event");
+      event.initEvent("input", false, true);
+      tag.dispatchEvent(event);
+    } else {
+      tag.dispatchEvent(new Event('input'));
+    }
+  }
+  
+  /**
+   * Event Input
+   * @param {String} input
+   * @return {String}
+   */
+  format(e){
+    const _value = e.target.value;
+    const _mask = this.state.mask || e.target.dataset.maskfy;
+    const _reverse = this.state.reverse || e.target.dataset.maskfyReverse==='true';
+    const _letters = this.state.letters || e.target.dataset.maskfyLetters==='true';
+    let _valueFormat;
+    if(!_mask) return console.error(new Error('Maskfy: Insert input mask required. Ex.: Maskfy({mask: "9999-99-99"})'));
+    if(_mask.length>=_value.length){
+      _valueFormat = _value.replace((_letters ? /\W/g : /\D/g), '').split('');
+    } else {
+      _valueFormat = _value.split('');
+      _valueFormat.pop();
+      if(e.target.selectionEnd!==_value.length){
+        setTimeout(() => this.event(e.target), 10);
+      }
+    }
+    if(/^\W+/.test(_mask) && _mask.length>=_value.length){
+      _valueFormat.unshift(/^\W+/.exec(_mask)[0]);
+    }
+    if(_reverse){
+      const _minSize = this.state.minSize || Number(e.target.dataset.maskfyMinsize);
+      const _maskReverse = _mask.split('');
+      const _valueReverse = _valueFormat.concat('');
+      _maskReverse.reverse();
+      _valueReverse.reverse().shift();
+      if(_minSize && _valueFormat.length<_minSize){
+        _valueReverse.push('0');
+      } else {
+        if(_valueReverse[_valueReverse.length-1]==='0'){
+          if(/\d/.test(_value[_value.length-1])){
+            _valueReverse.pop();
+          }
+        }
+      }
+      for(let i=0; i<_valueReverse.length; i++){
+        if(/\W/.test(_maskReverse[i])){
+          _valueReverse.splice(i, 0, _maskReverse[i]);
+        }
+      }
+      if(_maskReverse.length>=_valueReverse.length){
+        _valueFormat = _valueReverse.reverse();
+      }
+    } else {
+      for(let i=0; _mask.length>i && _mask.length>=_value.length; i++){
+        if(isNaN(parseInt(_mask[i])) && _valueFormat[i-1]){
+          _valueFormat.splice(i, 0, _mask[i])
+        }
+      }
+    }
+    _valueFormat.splice(_mask.length, _valueFormat.length);
+    e.target.value = _valueFormat.join('').replace(/(\W+)$/, '');
+    setTimeout(() => {
+      if(!e.target.classList.contains('maskfy--active')){
+        e.target.classList.add('maskfy--active');
+        this.event(e.target);
+      }
+      if(this.state.after){
+        this.formatAfter(e.target);
+      }
+    }, 10);
+  }
+
+  /**
+   * Event After Insert Value
+   * @param {String} input
+   * @return {String}
+   */
+  formatAfter(tag){
+    this.state.after(tag, this);
+  }
+  render(){
+    return this.props.children;
+  }
+}
